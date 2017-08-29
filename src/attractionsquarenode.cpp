f7f30619903b1d0f2004aeb8700f176d6fe46130
BM <bm@chinadog.com>
2017-08-29 11:03:09 +0300

    Изменено: grid

--- src/attractionsquarenode.cpp
+++ src/attractionsquarenode.cpp
@@ -45,12 +45,35 @@ int AttractionSquareNode::initTrack()
         node->getMaterial(6).setTexture( 0, texture("iron.jpg") );
         node->getMaterial(7).setTexture( 0, texture("rotor.png") );
         node->getMaterial(8).setTexture( 0, texture("rotor.png") );
-        node->getMaterial(9).setTexture( 0, texture("grid.png") );
-        node->getMaterial(0).TextureLayer[0].AnisotropicFilter = 16;
-        node->getMaterial(0).getTextureMatrix(0).setTextureScale(30,30);
-        node->getMaterial(9).MaterialType = video::EMT_TRANSPARENT_ALPHA_CHANNEL;
-        node->getMaterial(9).TextureLayer[0].AnisotropicFilter = 16;
-        node->getMaterial(9).getTextureMatrix(0).setTextureScale(100,100);
+        node->getMaterial(11).setTexture( 0, texture("grid.png") );
+log("-=-=-=-");
+log(node->getMaterialCount());
+//        node->getMaterial(0).TextureLayer[0].AnisotropicFilter = 16;
+//        node->getMaterial(0).getTextureMatrix(0).setTextureScale(30,30);
+        node->getMaterial(11).MaterialType = video::EMT_TRANSPARENT_ALPHA_CHANNEL;
+        node->getMaterial(11).TextureLayer[0].AnisotropicFilter = 16;
+        node->getMaterial(11).getTextureMatrix(0).setTextureScale(3,3);
+
+
+
+//        node->getMaterial(0).setTexture( 0, texture("metal_rzav.jpg") );
+//        node->getMaterial(1).setTexture( 0, texture("iron_green.jpg") );
+//        node->getMaterial(2).setTexture( 0, texture("iron_gray.jpg") );
+//        node->getMaterial(3).setTexture( 0, texture("iron_gray.jpg") );
+//        node->getMaterial(4).setTexture( 0, texture("iron_blue.jpg") );
+//        node->getMaterial(5).setTexture( 0, texture("iron_yellow.jpg") );
+//        node->getMaterial(6).setTexture( 0, texture("iron.jpg") );
+//        node->getMaterial(7).setTexture( 0, texture("rotor.png") );
+//        node->getMaterial(8).setTexture( 0, texture("rotor.png") );
+//        node->getMaterial(9).setTexture( 0, texture("iron.jpg") );
+//        node->getMaterial(10).setTexture( 0, texture("grid.png") );
+
+//        node->getMaterial(11).setTexture( 0, texture("russia.png") );
+//        node->getMaterial(12).setTexture( 0, texture("waterbump.png") );
+//        node->getMaterial(13).setTexture( 0, texture("waterbump.png") );
+//        node->getMaterial(14).setTexture( 0, texture("waterbump.png") );
+//        node->getMaterial(15).setTexture( 0, texture("waterbump.png") );
+//        node->getMaterial(16).setTexture( 0, texture("waterbump.png") );
 
 
         //node->addShadowVolumeSceneNode(0,-1,false,10000);
